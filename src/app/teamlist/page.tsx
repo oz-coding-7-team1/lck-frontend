85d9d55a0e83f4f575485551b4ca05cef6198c46
chanyangpraise <cychoi710@icloud.com>
2025-02-05 18:49:41 +0900

    ⚡️ Redirect to the corresponding player or team detail page when they are clicked in the search results.

--- src/app/teamlist/page.tsx
+++ src/app/teamlist/page.tsx
@@ -1,37 +1,37 @@
-import Link from "next/link";
-import TeamCard from "../../components/team/TeamCard";
-import { encodeTeamName } from "@/src/utils/urlUtils"; // 팀 이름을 URL-friendly하게 변환하는 함수
+import Link from 'next/link';
+import TeamCard from '../../components/team/TeamCard';
+import { encodeTeamName } from '@/src/utils/urlUtils'; // 팀 이름을 URL-friendly하게 변환하는 함수
 
 export default function TeamListPage() {
   const teams = [
     {
-      name: "T1",
-      rank: "1st",
-      logo: "/logos/t1.svg",
+      name: 'T1',
+      rank: '1st',
+      logo: '/logos/t1.svg',
       votes: 230000,
     },
     {
-      name: "GEN.G",
-      rank: "2nd",
-      logo: "/logos/geng.svg",
+      name: 'GEN.G',
+      rank: '2nd',
+      logo: '/logos/geng.svg',
       votes: 220000,
     },
     {
-      name: "Hanwha Life Esports",
-      rank: "3rd",
-      logo: "/logos/hle.svg",
+      name: 'Hanwha Life Esports',
+      rank: '3rd',
+      logo: '/logos/hle.svg',
       votes: 210000,
     },
     {
-      name: "Dplus KIA",
-      rank: "4th",
-      logo: "/logos/dk.svg",
+      name: 'Dplus KIA',
+      rank: '4th',
+      logo: '/logos/dk.svg',
       votes: 200000,
     },
     {
-      name: "kt Rolster",
-      rank: "5th",
-      logo: "/logos/kt.svg",
+      name: 'kt Rolster',
+      rank: '5th',
+      logo: '/logos/kt.svg',
       votes: 190000,
     },
   ];
